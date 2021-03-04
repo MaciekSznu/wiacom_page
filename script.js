7a7f013e48614584ed55a84e39671bbb858ede2a
Maciej Sznurawa <msznurawa@gmail.com>
2021-03-04 11:39:36 +0100

    Fixa some styling bugs, add images line

--- script.js
+++ script.js
@@ -20,13 +20,8 @@ const showMobileMenu = () => {
   mobileMenu.classList.toggle("navigation-visible");
 };
 
-// const hamburgerActive = () => {
-//   hamburgerButton.classList.toggle('hamburger-active');
-// };
-
 hamburgerButton.addEventListener("click", (e) => {
   showMobileMenu();
-  // hamburgerActive();
 });
 
 const mobileMenuItemsScroll = document.querySelectorAll(".navigation-item a");
@@ -48,13 +43,3 @@ mobileMenuItemsScroll.forEach((item, index) => {
     }
   });
 });
-
-// mobileMenuItems.forEach((item) => {
-//   click(item, () => {
-//     const width = window.innerWidth;
-//     if ( width < 1024 ) {
-//       showMobileMenu();
-//       hamburgerActive();
-//     }
-//   })
-// });
