7ca2054e9aee850fbe6601ab0e3d4c003ec54e30
Maciej Sznurawa <msznurawa@gmail.com>
2021-03-04 16:55:25 +0100

    Add Hero slider, fix footer & header paddings

--- script.js
+++ script.js
@@ -43,3 +43,26 @@ mobileMenuItemsScroll.forEach((item, index) => {
     }
   });
 });
+
+// SLIDER
+const mainPageSelector = document.querySelector(".hero-carousel");
+
+let homePageSliderInterval = setInterval(() => {
+  homePageSlider.next();
+}, 2500);
+
+const homePageSlider = new Siema({
+  selector: mainPageSelector,
+  loop: true,
+  duration: 750,
+  easing: "ease-out",
+  onChange() {
+    clearInterval(homePageSliderInterval);
+    homePageSliderInterval = setInterval(() => homePageSlider.next(), 2500);
+  },
+});
+
+const prev = document.querySelector(".hero-control-prev");
+const next = document.querySelector(".hero-control-next");
+prev.addEventListener("click", () => homePageSlider.prev());
+next.addEventListener("click", () => homePageSlider.next());
