66979d1ec77bec8cdc1f5d6c1402a4353c1dd3ec
Maciej Sznurawa <msznurawa@gmail.com>
2021-03-05 17:04:15 +0100

    Change some styles & photos according to customer vision

--- script.js
+++ script.js
@@ -47,19 +47,19 @@ mobileMenuItemsScroll.forEach((item, index) => {
 // SLIDER
 const mainPageSelector = document.querySelector(".hero-carousel");
 
-let homePageSliderInterval = setInterval(() => {
-  homePageSlider.next();
-}, 2500);
+// let homePageSliderInterval = setInterval(() => {
+//   homePageSlider.next();
+// }, 2500);
 
 const homePageSlider = new Siema({
   selector: mainPageSelector,
   loop: true,
   duration: 750,
   easing: "ease-out",
-  onChange() {
-    clearInterval(homePageSliderInterval);
-    homePageSliderInterval = setInterval(() => homePageSlider.next(), 2500);
-  },
+  // onChange() {
+  //   clearInterval(homePageSliderInterval);
+  //   homePageSliderInterval = setInterval(() => homePageSlider.next(), 2500);
+  // },
 });
 
 const prev = document.querySelector(".hero-control-prev");
